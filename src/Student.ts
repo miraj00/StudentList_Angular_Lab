3e912c718385c24980f307c6a2db5bd2de8b6c72
miraj <miraj00@gmail.com>
2023-03-02 21:32:46 -0500

    ng if and ng for used in component

---
+++ src/Student.ts
@@ -0,0 +1,10 @@
+// Angular likes your models to be interfaces that are a series of properties
+
+export interface Student {
+    name : string;
+    age : number;
+    major : string;
+    isPassing : boolean;
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Student } from 'src/Student';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// anything in this classs is useable in the matching view
 export class AppComponent {
   title = 'intro';
+
+  index : number = 10;
+
+  jimmy : Student = {age :18, major: "Biology", name: "Jimmy Buffet", isPassing :true}
+
+  students: Student[] = [ this.jimmy, 
+                          {name : " hannah Monsanto", major : "Toxicology", age : 1000, isPassing: true},
+                          {name:"james", major :"History", age:25, isPassing:false }]
+
 }
